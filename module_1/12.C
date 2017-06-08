e04be2e6bec1b1644357fca24c676fcd05191351
myagkiym <mihailmyagkij@yandex.ru>
2017-06-08 22:03:47 +0300

    Create 12.C

---
+++ module_1/12.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	int o, a[100];
+	char b[100];
+	char max = a[0];
+	gets(b);
+	for (o = 0; b[i] != '\0'; o++)
+		a[o] = b[o];
+	for (o; o >= 0; o--)
+	{
+		if (a[o]>max)
+			max = a[o];
+	}
+	printf("max code is %o\n", max);
+}
