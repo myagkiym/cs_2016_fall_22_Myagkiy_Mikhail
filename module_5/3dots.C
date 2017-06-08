05d72c85cb81849044800094f309ed2ba74ab69e
myagkiym <mihailmyagkij@yandex.ru>
2017-06-08 22:01:41 +0300

    Create 3dots.C

--- module_5/3dots.C
+++ module_5/3dots.C
@@ -14,25 +14,25 @@ int main()
 	float V2 = 0;
 
 	char s;
-	printl("Vvedite tochnost znacheniy(Max 9): ");
-	scanl("%c", &s);
+	printf("Vvedite tochnost znacheniy(Max 9): ");
+	scanf("%c", &s);
 	char string[] = "%.zl\n";
 	string[2] = s;
-	printl("Lower border: ");
-	scanl("%l", &a);
-	printl("Upper border: ");
-	scanl("%l", &b);
-	printl("Enter your step: ");
-	scanl("%l", &p);
+	printf("Lower border: ");
+	scanf("%l", &a);
+	printf("Upper border: ");
+	scanf("%l", &b);
+	printf("Enter your step: ");
+	scanf("%l", &p);
 	x = a;
 	V1 = (1 / (2 * p))*((-3) * L(x) + 4 * (L(x + p)) - (L(x + 2 * p)));
 	V2 = (1 / (2 * p))*(-L(x) + L(x + 2 * p));
-	printl(string, V1);
-	printl(string, V2);
+	printf(string, V1);
+	printf(string, V2);
 	while (x + 2 * p <= b) {
 		Res = (1 / (2 * p))*(L(x) - 4 * (L(x + p)) + 3 * (L(x + 2 * p)));
 		x = x + p;
-		printl(string, Res);
+		printf(string, Res);
 	}
 	return 0;
 }
